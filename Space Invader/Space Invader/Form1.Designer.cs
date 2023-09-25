83100f4341c68a8714b7536136df7af6b80299f1
ltom <tom.liere@ynov.com>
2023-09-25 17:19:28 +0200

    partie 1

---
+++ Space Invader/Space Invader/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Space_Invader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(12, 528);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(94, 24);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score : 0";
+            // 
+            // player
+            // 
+            this.player.Image = global::Space_Invader.Properties.Resources.triangle;
+            this.player.Location = new System.Drawing.Point(330, 504);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(59, 48);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Space Invader";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Space Invader/Space Invader/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Invader
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight;
+        int playerSpeed = 12;
+        int enemySpeed = 5;
+        int score = 0;
+        int enemyBulletTimer = 300;
+
+        PictureBox[] sadInvadersArray;
+
+        bool shooting;
+        bool isGameOver;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+                makeBullet("bullet");
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                removeAll();
+                gameSetup();
+            }
+        }
+
+        private void makeInvaders() 
+        {
+            sadInvadersArray = new PictureBox[15];
+
+            int left = 0;
+
+            for(int i = 0; i < sadInvadersArray.Length; i++) 
+            {
+                sadInvadersArray[i] = new PictureBox();
+                sadInvadersArray[i].Size = new Size(60, 50);
+                sadInvadersArray[i].Image = Properties.Resources.sadFace;
+                sadInvadersArray[i].Top = 5;
+                sadInvadersArray[i].Tag = "sadInvaders";
+                sadInvadersArray[i].Left = left;
+                sadInvadersArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(sadInvadersArray[i]);
+                left = left - 80;
+            }
+        
+        }
+
+        private void gameSetup()
+        {
+            txtScore.Text = "Score : 0";
+            score = 0;
+            isGameOver = false;
+
+            enemyBulletTimer = 300;
+            enemySpeed = 5;
+            shooting = false;
+
+            makeInvaders();
+            gameTimer.Start();
+
+        }
+
+        private void gameOver(string message)
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            txtScore.Text = "Score : " + score + " " + message;
+        
+        }
+
+        private void removeAll() 
+        {
+
+            foreach(PictureBox i in sadInvadersArray) 
+            {
+                this.Controls.Remove(i);
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "bullet" || (string)x.Tag == "sadBullet")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+                
+            }
+
+        }
+
+        private void makeBullet(string bulletTag)
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = Properties.Resources.bullet;
+            bullet.Size = new Size(5, 20);
+            bullet.Tag = bulletTag;
+            bullet.Left = player.Left + player.Width / 2;
+
+            if ((string)bullet.Tag == "bullet")
+            {
+                bullet.Top = player.Top - 20;     
+            }
+            else if ((string)bullet.Tag == "sadBullet")
+            {
+                bullet.Top = -100;
+            }
+
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
+
+
+        }
+    }
+}
